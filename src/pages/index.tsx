33f2d6ec725ae2883d352ebf30b7ebde9eec554c
omar-aziz <omars@hey.com>
2022-01-16 09:59:38 -0600

    copy fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -179,7 +179,7 @@ const Home: NextPage = () => {
           <button
             type="button"
             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 flex space-x-2 text-md"
-            onClick={copied ? () => setCopied(false) : copy}
+            onClick={copied ? () => setCopied(false) : copyAddress}
           >
             <span>Copy Address</span>
 
